8909d1b84b2b68b5f585d4b4d55413eb9ce0d3a6
athonbil <lendariosupersaiyajin7@proton,me>
2026-04-01 21:21:54 -0300

    cachorro v2 com DAO

--- Controllers/AnimalsController.php
+++ Controllers/AnimalsController.php
@@ -3,6 +3,7 @@ require __DIR__ . '/../Models/Model_Cachorro.php';
 require __DIR__ . '/../Dao/Cachorro.php';
 class AnimalsController
 {
+    // simulei uma lista de cachorros para exemplificar a funcionalidade, professor disse que tava tudo bem. 
     public function listarCachorros()
     {
         $cachorros = [
@@ -17,4 +18,4 @@ class AnimalsController
     }
 }
 
-?>
+?>  
--- Dao/Cachorro.php
+++ Dao/Cachorro.php
@@ -1,5 +1,18 @@
 <?php
 class Cachorro extends Model_Cachorro
 {
+    private $db;
+    public function  __construct($db)
+    {
+        $this->db = $db;    
+    }
+    public function criarCachorro($nome, $raca, $porte){
+        $sql = "INSERT INTO cachorros (nome, raca, porte) VALUES (:nome, :raca, :porte)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':raca', $raca);
+        $stmt->bindParam(':porte', $porte);
+        return $stmt->execute();
+    }
 }
 ?>
